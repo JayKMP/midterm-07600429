8c30494fde14634627199710b65a4640c92cbbf2
JJ <Jaykamolneth@hotmail.com>
2020-09-06 12:32:17 +0700

    mid

--- app/src/main/java/th/ac/su/cp/speedcalculator/MainActivity.java
+++ app/src/main/java/th/ac/su/cp/speedcalculator/MainActivity.java
@@ -3,6 +3,10 @@ package th.ac.su.cp.speedcalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+import  android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        Button calB =findViewById(R.id.cal);
+        calB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText meterB = findViewById(R.id.km);
+                EditText secondB = findViewById(R.id.time);
+                String km = meterB.getText().toString();
+                String time = secondB.getText().toString();
+
+                if(km.isEmpty() || time.isEmpty()){
+                    Toast t = Toast.makeText(MainActivity.this,
+                            "Distance and time are required",
+                            Toast.LENGTH_LONG);
+                    t.show();
+
+                }
+            }
+
+        });
+
+    };
 }
